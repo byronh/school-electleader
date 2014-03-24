739129ec0eb43c1d3aa2b03524781e43b87cd618
byronh <byronh@gmail.com>
2014-03-24 01:44:25 -0400

    Tweaks

--- electleader.c
+++ electleader.c
@@ -1,30 +1,34 @@
 /*
 Byron Henze
 66809088
-Assignment 3 - leader election
+Assignment 3 - leader election using Hirshberg-Sinclair (HS) algorithm
 */
 
 #include "mpi.h"
 #include <stdio.h>
 
+#define T_ELECTION 0
+#define T_REPLY 1
+
 // Usage: mpiexec -n NUM ./electleader PNUM
 int main(int argc, char* argv[]) {
 	
 	int rank, num;
+	int msgs_sent = 0;
+	int msgs_recvd = 0;
 
+	// Initialize rank and uid
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &num);
 	int pnum = atoi(argv[argc-1]);
 	int uid = ((rank + 1) * pnum) % num;
 
-	int messages_sent = 0;
-	int messages_received = 0;
-
-	int rank_left = (rank - 1) % num;
-	if (rank_left < 0) rank_left += num;
-	int rank_right = (rank + 1) % num;
-	if (rank_right < 0) rank_right += num;
+	// Initialize neighbor ranks
+	int lrank = (rank - 1) % num;
+	if (lrank < 0) lrank += num;
+	int rrank = (rank + 1) % num;
+	if (rrank < 0) rrank += num;
 
 	/*
 	MPI_Send(void* data, int count, MPI_Datatype datatype, int destination,
@@ -36,39 +40,32 @@ int main(int argc, char* argv[]) {
 	*/
 
 	int phase = 0;
-	int distance = 0;
+	int dist = 0;
 
-	int leftSend[3] = {uid, phase, distance};
-	int rightSend[3] = {uid, phase, distance};
-	int leftRecv[3] = {0, 0, 0};
-	int rightRecv[3] = {0, 0, 0};
+	int lsend[3] = {uid, phase, dist};
+	int rsend[3] = {uid, phase, dist};
+	int lrecv[3] = {0, 0, 0};
+	int rrecv[3] = {0, 0, 0};
 
-	MPI_Request requestLeft, requestRight;
-	MPI_Status statusLeft, statusRight;
+	MPI_Request lreq, rreq;
+	MPI_Status lstat, rstat;
 
 	// Phase 0 election
-	MPI_Isend(&leftSend, 3, MPI_INT, rank_left, 0, MPI_COMM_WORLD, &requestLeft);
-	messages_sent++;
-	//printf("Process %d send left message to process %d\n", rank, rank_left);
-	MPI_Wait(&requestLeft, &statusLeft);
+	MPI_Isend(&lsend, 3, MPI_INT, lrank, T_ELECTION, MPI_COMM_WORLD, &lreq);
+	MPI_Isend(&rsend, 3, MPI_INT, rrank, T_ELECTION, MPI_COMM_WORLD, &rreq);
+	msgs_sent += 2;
+	MPI_Wait(&lreq, &lstat);
+	MPI_Wait(&rreq, &rstat);
 
-	MPI_Isend(&rightSend, 3, MPI_INT, rank_right, 0, MPI_COMM_WORLD, &requestRight);
-	messages_sent++;
-	//printf("Process %d send right message to process %d\n", rank, rank_right);
-	MPI_Wait(&requestRight, &statusRight);
-
-	MPI_Recv(&leftRecv, 3, MPI_INT, rank_left, 0, MPI_COMM_WORLD, &statusLeft);
-	messages_received++;
-	printf("Process %d received left message from process %d\n", rank, rank_left);
-
-	MPI_Recv(&rightRecv, 3, MPI_INT, rank_right, 0, MPI_COMM_WORLD, &statusRight);
-	printf("Process %d received right message from process %d\n", rank, rank_right);
-	messages_received++;
+	MPI_Recv(&lrecv, 3, MPI_INT, lrank, T_ELECTION, MPI_COMM_WORLD, &lstat);
+	MPI_Recv(&rrecv, 3, MPI_INT, rrank, T_ELECTION, MPI_COMM_WORLD, &rstat);
+	msgs_recvd += 2;
+	printf("%d received L election from %d: {%d, %d, %d}\n", rank, lrank, lrecv[0], lrecv[1], lrecv[2]);
+	printf("%d received R election from %d: {%d, %d, %d}\n", rank, rrank, rrecv[0], rrecv[1], rrecv[2]);
 
 
-	// End
-	printf("rank=%d, id=%d, leader=0, mrcvd=%d, msent=%d\n", rank, uid,
-		messages_received, messages_sent);
+	// Election complete
+	//printf("rank=%d, id=%d, leader=0, mrcvd=%d, msent=%d\n", rank, uid, msgs_recvd, msgs_sent);
 
 	MPI_Finalize();
 	return 0;
