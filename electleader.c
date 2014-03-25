b4da5778470e71ce51ffe8841a615adab6c0385c
byronh <byronh@gmail.com>
2014-03-25 01:25:33 -0400

    Tweaks

--- electleader.c
+++ electleader.c
@@ -31,6 +31,8 @@ Upon receiving a message REPLY, j, k from left (right):
 #define TRUE 1
 #define FALSE 0
 
+#define DEBUG 0
+
 #define T_NULL 0
 #define T_ELECTION 1
 #define T_REPLY 2
@@ -109,101 +111,120 @@ int main(int argc, char* argv[]) {
 	send_msg(lsend, lrank, lreq, lstat);
 	send_msg(rsend, rrank, rreq, rstat);
 
-	int i=0;
+	int t, j, k, d;
+
 	while (!election_complete) {
 		lsent = 0;
 		rsent = 0;
-		recv_msg(lrecv, lrank, lstat);
-		recv_msg(rrecv, rrank, rstat);
 
-		int t = lrecv[I_TYPE];
-		int j = lrecv[I_J];
-		int k = lrecv[I_K];
-		int d = lrecv[I_D];
+		if (ldone || rdone) election_complete = TRUE;
 
-		// Temp: process 0 announce self as leader
-		if (uid == 0 && i>4) {
-			int data[4] = {T_LEADER, uid, -1, -1};
-			send_left(data);
-			send_right(data);
-			break;
-		}
+		if (!election_complete) {
+			recv_msg(lrecv, lrank, lstat);
+			
+			t = lrecv[I_TYPE];
+			j = lrecv[I_J];
+			k = lrecv[I_K];
+			d = lrecv[I_D];
 
-		// Handle messages from the left
-		if (t == T_LEADER) {
-			ldone = TRUE;
-			leader = j;
-			printf("** %d acknowledges leader %d\n", uid, leader);
-			if (!rdone) {
-				int data[4] = {T_LEADER, leader, -1, -1};
-				send_right(data);
-			}
-			//break;
-		} else if (t == T_ELECTION) {
-			if (!rdone && (j > uid) && (d <= ipow(2, k))) {
-				int election[4] = {T_ELECTION, j, k, d + 1};
-				send_right(election);
-			}
-			if (!ldone && (j > uid) && (d == ipow(2, k))) {
-				int reply[4] = {T_REPLY, j, k, -1};
-				send_left(reply);
-			}
-		} else if (t == T_REPLY) {
-			if (!rdone && uid != j) {
-				replies[j][k] = TRUE;
-				int reply[4] = {T_REPLY, j, k, -1};
-				send_right(reply);
-			}
-		}
-
-		t = rrecv[I_TYPE];
-		j = rrecv[I_J];
-		k = rrecv[I_K];
-		d = rrecv[I_D];
-
-		// Handle messages from the right
-		if (t == T_LEADER) {
-			rdone = TRUE;
-			leader = j;
-			printf("** %d acknowledges leader %d\n", uid, leader);
-			if (!ldone) {
-				int data[4] = {T_LEADER, leader, -1, -1};
-				send_left(data);
-			}
-			//break;
-		} else if (t == T_ELECTION) {
-			if (!ldone && (j > uid) && (d <= ipow(2, k))) {
-				int election[4] = {T_ELECTION, j, k, d + 1};
-				send_left(election);
-			}
-			if (!rdone && (j > uid) && (d == ipow(2, k))) {
-				int reply[4] = {T_REPLY, j, k, -1};
-				send_right(reply);
-			}
-		} else if (t == T_REPLY) {
-			if (!ldone && uid != j) {
-				replies[j][k] = TRUE;
-				int reply[4] = {T_REPLY, j, k, -1};
-				send_left(reply);
+			// Handle messages from the left
+			if (t == T_LEADER && leader == -1) {
+				ldone = TRUE;
+				leader = j;
+				if (DEBUG) printf("** %d acknowledges leader %d\n", uid, leader);
+				if (!rdone) {
+					int data[4] = {T_LEADER, leader, -1, -1};
+					send_right(data);
+				}
+			} else if (t == T_ELECTION) {
+				if (!rdone && (j > uid) && (d <= ipow(2, k))) {
+					int election[4] = {T_ELECTION, j, k, d + 1};
+					send_right(election);
+				}
+				else if (!ldone && (j > uid) && (d == ipow(2, k))) {
+					int reply[4] = {T_REPLY, j, k, -1};
+					send_left(reply);
+				}
+				if (uid == j) {
+					leader = uid;
+					int announce[4] = {T_LEADER, uid, -1, -1};
+					send_left(announce);
+					send_right(announce);
+					ldone = TRUE;
+					rdone = TRUE;
+				}
+			} else if (t == T_REPLY) {
+				if (uid != j) {
+					replies[j][k] = TRUE;
+					int reply[4] = {T_REPLY, j, k, -1};
+					if (!rdone) send_right(reply);
+				}
+				if (replies[j][k] == TRUE) {
+					printf("%d to next phase\n", uid);
+					int election[4] = {T_ELECTION, j, k + 1, 1};
+					if (!ldone) send_left(election);
+					if (!rdone) send_right(election);
+				}
 			}
 		}
 
 		if (ldone || rdone) election_complete = TRUE;
-		i++;
 
-		// Send null messages to prevent deadlock
-		if (lsent == 0) {
-			int data[4] = {T_NULL, uid, 0, 0};
-			send_left(data);
-		}
-		if (rsent == 0) {
-			int data[4] = {T_NULL, uid, 0, 0};
-			send_right(data);
+		if (!election_complete) {
+			recv_msg(rrecv, rrank, rstat);
+			t = rrecv[I_TYPE];
+			j = rrecv[I_J];
+			k = rrecv[I_K];
+			d = rrecv[I_D];
+
+			// Handle messages from the right
+			if (t == T_LEADER && leader == -1) {
+				rdone = TRUE;
+				leader = j;
+				if (DEBUG) printf("** %d acknowledges leader %d\n", uid, leader);
+				if (!ldone) {
+					int data[4] = {T_LEADER, leader, -1, -1};
+					send_left(data);
+				}
+			} else if (t == T_ELECTION) {
+				if (!ldone && (j > uid) && (d <= ipow(2, k))) {
+					int election[4] = {T_ELECTION, j, k, d + 1};
+					send_left(election);
+				}
+				else if (!rdone && (j > uid) && (d == ipow(2, k))) {
+					int reply[4] = {T_REPLY, j, k, -1};
+					send_right(reply);
+				}
+				if (uid == j) {
+					leader = uid;
+					int announce[4] = {T_LEADER, uid, -1, -1};
+					send_left(announce);
+					send_right(announce);
+					ldone = TRUE;
+					rdone = TRUE;
+				}
+			} else if (t == T_REPLY) {
+				if (uid != j) {
+					replies[j][k] = TRUE;
+					int reply[4] = {T_REPLY, j, k, -1};
+					if (!ldone) send_left(reply);
+				}
+				if (replies[j][k] == TRUE) {
+					printf("%d to next phase\n", uid);
+					int election[4] = {T_ELECTION, j, k + 1, 1};
+					if (!ldone) send_left(election);
+					if (!rdone) send_right(election);
+				}
+			}
 		}
 	}
 
 	// Election complete
-	//printf("rank=%d, id=%d, leader=0, mrcvd=%d, msent=%d\n", rank, uid, msgs_recvd, msgs_sent);
+	printf("rank=%d, id=%d, leader=%d, mrcvd=%d, msent=%d\n", rank, uid, leader == uid ? 1 : 0, msgs_recvd, msgs_sent);
+
+	// Another messaging round to send total messages to leader
+
+
 
 	MPI_Finalize();
 	return 0;
@@ -251,7 +272,7 @@ void recv_msg(int* data, int source, MPI_Status status) {
 
 // Prints debug information about a sent message
 void print_sent_msg(int send[], int dest) {
-	if (send[I_TYPE] == T_NULL) return;
+	if (!DEBUG || send[I_TYPE] == T_NULL) return;
 	printf("%d sent to %d %s: {%d, uid:%d, phase:%d, dist:%d}\n", uid, dest,
 		dest == lrank ? "L" : "R",
 		send[I_TYPE], send[I_J], send[I_K], send[I_D]);
@@ -259,7 +280,7 @@ void print_sent_msg(int send[], int dest) {
 
 // Prints debug information about a received message
 void print_recv_msg(int recv[], int source) {
-	if (recv[I_TYPE] == T_NULL) return;
+	if (!DEBUG || recv[I_TYPE] == T_NULL) return;
 	printf("%d received from %d %s: {%d, uid:%d, phase:%d, dist:%d}\n", uid, source,
 		source == lrank ? "L" : "R",
 		recv[I_TYPE], recv[I_J], recv[I_K], recv[I_D]);
